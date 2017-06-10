1e8886a623ff47a4bab63a2a2f5bff246199ae84
George Berezhnoy <gohabereg@users.noreply.github.com>
2017-06-10 22:44:21 +0300

    Add get method for events

--- hawk/models/events.js
+++ hawk/models/events.js
@@ -2,18 +2,33 @@ module.exports = (function() {
 
   let mongo = require('../modules/database');
 
-  let add = function(collection, events) {
+  /**
+   * Add new event to domain collection
+   *
+   * @param domain
+   * @param events
+   */
+  let add = function(domain, event) {
 
-    mongo.insertOne(collection, events)
-      .then(function(){
-        return true;
-      }).catch(function(){
-        return false;
-      });
+    return mongo.insertOne(domain, event);
+
+  };
+
+  /**
+   * Get domain events by query
+   *
+   * @param domain
+   * @param query
+   */
+  let get = function (domain, query) {
+
+    return mongo.find(domain, query);
+
   };
 
   return {
     add: add,
+    get: get
   };
 
 })();
