37d4179824e542140a37a0d6f0bf3c9527c74338
N0str <ntpcp@yandex.ru>
2017-06-10 16:56:22 +0300

    Add creation of tokens

--- hawk/routes/apps.js
+++ hawk/routes/apps.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require("../modules/database");
 
 /* Show page for new app registration */
 router.get('/create', function(req, res, next) {
@@ -11,26 +12,47 @@ router.post('/create', function(req, res, next) {
 
     var name = req.body.app_name;
 
-    if (checkApplicationName(name)) {
-        var uuid = require('uuid');
-        var token = uuid.v4();
-        res.render('token', { title: 'Your token', token: token });
-    }
-    else {
-        res.render('token', { title: 'Your token', error: "Invalid name" });
-    }
+    /* Check if application is already exists */
+    checkApplicationName(name).then(function(result) {
+
+        /* if not exists -> generate token and add to DB*/
+        if (result) {
+            var uuid = require('uuid');
+            var token = uuid.v4();
+            addNewApplication(name, token);
+            res.render('token', {title: 'Get your token', token: token});
+        }
+        else {
+            res.render('token', {title: 'Error', error: "User already exists"});
+        }
+    })
 
 });
 
-
+/* Return if application with name specified is not exists */
 function checkApplicationName(name) {
-    console.log(name, name == '', name.length);
-    if (name != "") {
-        return true;
-    }
-    else {
-        return false;
-    }
+    return mongo.findOne('hawk_applications', {'name': name})
+        .then(function (result) {
+            if (result) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        });
+}
+
+/* Add new application and token to DB */
+function addNewApplication(app_name, token) {
+    return mongo.insertOne('hawk_applications', {'name': app_name, 'token': token})
+        .then(function (result) {
+            if (result) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        });
 }
 
 
