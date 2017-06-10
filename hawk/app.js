0ecef5f07313e8506df6d3e1a1eb18e3d735a728
George Berezhnoy <gohabereg@users.noreply.github.com>
2017-06-10 16:52:59 +0300

    Garage v0

--- hawk/app.js
+++ hawk/app.js
@@ -9,6 +9,7 @@ var config = require('./config');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var apps = require('./routes/apps');
+var garage = require('./routes/garage');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/app', apps);
+app.use('/garage', garage);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
