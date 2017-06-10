65df3435f320d64c78f6d12ccb88d55383396ff2
Murod Khaydarov <murod.haydarov@gmail.com>
2017-06-10 09:33:24 +0300

    package json & server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log('here')
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 80;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://hawk.io:' + PORT);
