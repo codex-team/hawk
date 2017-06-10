3750fe9908a0b7b8936dd1fad0c348955ead2430
Murod Khaydarov <murod.haydarov@gmail.com>
2017-06-10 12:27:22 +0300

    special route for static files

--- hawk/public/build/bundle.js
+++ hawk/public/build/bundle.js
@@ -64,23 +64,17 @@ var hawk =
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
 /**
 * Require CSS build
 */
-__webpack_require__(0);
+__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"../stylesheets/hawk.css\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 
 hawk = (function (hawk) {
 
