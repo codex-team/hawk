918f1500e2fbe8a2f23d71b39a97aa59f9bffbd6
George Berezhnoy <gohabereg@users.noreply.github.com>
2017-06-17 00:09:42 +0300

    Docs

--- hawk/models/notifies.js
+++ hawk/models/notifies.js
@@ -4,10 +4,27 @@ let email = require('../modules/email');
 
 module.exports = function () {
 
+  const templatesPath = 'views/notifies/';
+  const templates = {
+    messenger: 'messenger.twig',
+    email: 'email.twig'
+  };
+
+  /**
+   * Send notifications about new event using user settings
+   *
+   * Available services
+   * - Telegram
+   * - Slack
+   * - Email
+   *
+   * @param user
+   * @param domain
+   * @param event
+   */
   let send = function (user, domain, event) {
 
-
-    Twig.renderFile('views/notifies/messenger.twig', {event: event, domain: domain}, function (err, html) {
+    Twig.renderFile(templatesPath + templates.messenger, {event: event, domain: domain}, function (err, html) {
 
       if (err) {
         console.log('Can not render notify template because of ', err);
@@ -25,7 +42,7 @@ module.exports = function () {
 
     if (user.notifies.email) {
 
-      Twig.renderFile('views/notifies/email.twig', {event: event, domain: domain}, function (err, html) {
+      Twig.renderFile(templatesPath + templates.email, {event: event, domain: domain}, function (err, html) {
         email.init();
         email.send(
           user.email,
