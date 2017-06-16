0ea870ad1f81cd7249f4165a171763b4919bf708
Murod Khaydarov <murod.haydarov@gmail.com>
2017-06-16 17:29:06 +0300

    aslant configuration

--- hawk/webpack.config.js
+++ hawk/webpack.config.js
@@ -17,24 +17,48 @@ module.exports = {
         use : "file-loader?name=[path][name].[ext]"
       },
       {
+        /**
+         * Use for all CSS files loaders below
+         * - extract-text-webpack-plugin
+         * - postcss-loader
+         */
         test: /\.css$/,
+        /** extract-text-webpack-plugin */
         use: ExtractTextPlugin.extract([
-          {
-            loader: 'css-loader',
-            options: {
-              minimize: 1,
-              importLoaders: 1
-            }
-          },
-          'postcss-loader'
-        ])
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: 1,
+                importLoaders: 1
+              }
+            },
+            /** postcss-loader */
+            'postcss-loader'
+          ])
       },
       {
+        /**
+         * Use for all JS files loaders below
+         * - babel-loader
+         * - eslint-loader
+         */
         test: /\.js$/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['env']
-        }
+        use : [
+          /** Babel loader */
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env']
+            },
+          },
+          /** ES lint For webpack build */
+          {
+            loader: "eslint-loader",
+            options: {
+              fix: false
+            }
+          }
+        ]
       }
     ]
   },
