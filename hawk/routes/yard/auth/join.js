1b85f79bb7959554a6b9f50c2b2c22f418cb49e2
Taly Guryn <vitalik7tv@yandex.ru>
2017-06-23 21:53:32 +0300

    Check email uniqness on join page

--- hawk/routes/yard/auth/join.js
+++ hawk/routes/yard/auth/join.js
@@ -14,8 +14,10 @@ let join = {
     user.current(req).then(function (found) {
 
       if (found) {
+
         res.redirect('/garage');
         return;
+
       }
 
       res.render('yard/auth/join');
@@ -28,21 +30,41 @@ let join = {
   post: function (req, res, next) {
 
     user.current(req).then(function (found) {
+
       if (found) {
+
         res.redirect('/garage');
         return;
+
       }
 
       let email = req.body.email;
 
-      user.add(email).then(function (insertedUser) {
-          if (insertedUser) {
-            auth.authUser(res, insertedUser);
-            res.redirect('/garage');
-          } else {
-            res.render('error', { message: 'Try again later.' });
-          }
-      }).catch(console.log);
+      user.checkParamUniqness({email: email})
+        .then(function (isEmailExist) {
+
+          return user.add(email)
+            .then(function (insertedUser) {
+
+              if (insertedUser) {
+
+                auth.authUser(res, insertedUser);
+                res.redirect('/garage');
+
+              } else {
+
+                res.render('error', { message: 'Try again later.' });
+
+              }
+
+            }).catch(console.log);
+
+        }).catch(function () {
+
+          res.render('error', { message: 'User with this email ia already registered. Enter the other email or try to remember password.' });
+          return;
+
+        });
 
     });
 
