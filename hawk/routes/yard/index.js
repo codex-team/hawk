c7022a0fc629fbfe12faea11511ad991bfda1063
Taly Guryn <vitalik7tv@yandex.ru>
2017-06-30 21:33:23 +0300

    Update meta

--- hawk/routes/yard/index.js
+++ hawk/routes/yard/index.js
@@ -3,16 +3,31 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/**
+ * Home page
+ */
 router.get('/', function (req, res, next) {
 
   res.render('yard/index');
 
 });
 
+
+/**
+ * Docs page
+ */
 router.get('/docs', function (req, res, next) {
 
-  res.render('yard/docs/index');
+  res.render('yard/docs/index', {
+
+    main: {
+
+      title : 'Hawk.so Docs',
+      description : 'Hawk.so is a clever and easy-to-use error tracker. It helps improve your applications. To learn how to start using Hawk..'
+
+    }
+
+  });
 
 });
 
