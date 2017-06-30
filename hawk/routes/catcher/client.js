43aab4808a329f0e02d3a2c91e1389856f60ffc8
George Berezhnoy <gohabereg@users.noreply.github.com>
2017-07-01 02:52:56 +0300

    Client stack parse

--- hawk/routes/catcher/client.js
+++ hawk/routes/catcher/client.js
@@ -33,7 +33,7 @@ let connection = function (ws) {
       message       : message.message,
       errorLocation : message.error_location,
       location      : message.location,
-      stack         : message.stack,
+      stack         : parseStack(message),
       groupHash     : md5(location),
       userAgent     : message.navigator,
       time          : Math.floor(message.time / 1000)
@@ -69,7 +69,7 @@ let connection = function (ws) {
       })
       .catch( function (e) {
 
-        ws.send(JSON.stringify({type: 'error', message: e.message}))
+        ws.send(JSON.stringify({type: 'error', message: e.message}));
 
       });
 
@@ -87,3 +87,143 @@ let connection = function (ws) {
 };
 
 reciever.on('connection', connection);
+
+let parseStack = function (event) {
+
+  const REGEXPS = {
+    FF_SAFARI_OPERA_11: /(.*)@(\S+)\:(\d+):(\d+)/,
+    CHROME_IE: /^\s*at (.*) \((\S+):(\d+):(\d+)\)/m,
+    SAFARI_NATIVE: /^(eval@)?(\[native code\])?$/,
+    OPERA_9: /Line (\d+).*script (?:in )?(\S+)/i,
+    OPERA_10: /Line (\d+).*script (?:in )?(\S+)(?:: In function (\S+))?$/i,
+  };
+
+  let stack = event.stack;
+
+  let parseChromeIE = function () {
+
+    let filtered = stack.split('\n').filter(function (line) {
+
+      return REGEXPS.CHROME_IE.test(line) && !/^Error created at/.test(line);
+
+    });
+
+    return filtered.map(function (line) {
+
+      if (/eval/.test(line)) {
+
+        /* Replace eval calls to expected format */
+        line = line.replace(/eval \(/, '').replace(/, .*:\d+:\d+\)/, '');
+
+      }
+
+      let matches = REGEXPS.CHROME_IE.exec(line);
+
+      return {
+        func: matches[1],
+        file: matches[2],
+        line: matches[3],
+        col: matches[4]
+      };
+
+    });
+
+  };
+  let parseSafariOpera11FF = function () {
+
+    let filtered = stack.split('\n').filter(function (line) {
+
+      return !/^Error created at/.test(line) && !REGEXPS.SAFARI_NATIVE.test(line) && REGEXPS.FF_SAFARI_OPERA_11.test(line);
+
+    });
+
+    return filtered.map(function (line) {
+
+      let matches = REGEXPS.FF_SAFARI_OPERA_11.exec(line);
+
+      return {
+        func: matches[1],
+        file: matches[2],
+        line: matches[3],
+        col: matches[4]
+      };
+
+    });
+
+  };
+  let parseOpera9 = function () {
+
+    let filtered = event.message.split('\n').filter(function (line) {
+
+      return REGEXPS.OPERA_9.test(line);
+
+    });
+
+    return filtered.map(function (line) {
+
+      let matches = REGEXPS.OPERA_9.exec(line);
+
+      return {
+        file: matches[2],
+        line: matches[1]
+      };
+
+    });
+
+  };
+
+  let parseOpera10 = function () {
+
+    let filtered = stack.split('\n').filter(function (line) {
+
+      return REGEXPS.OPERA_10.test(line);
+
+    });
+
+
+    return filtered.map(function (line) {
+
+      let matches = REGEXPS.OPERA_9.exec(line);
+
+      return {
+        func: matches[3] || undefined,
+        file: matches[2],
+        line: matches[1]
+      };
+
+    });
+
+  };
+
+  if (REGEXPS.OPERA_9.test(event.message)) {
+
+    stack = parseOpera9();
+
+  } else if (REGEXPS.OPERA_10.test(stack)) {
+
+    stack = parseOpera10();
+
+  } else if (REGEXPS.CHROME_IE.test(stack)) {
+
+    stack = parseChromeIE();
+
+  } else if (REGEXPS.FF_SAFARI_OPERA_11.test(stack)) {
+
+    stack = parseSafariOpera11FF();
+
+  } else {
+
+    console.log('Can\'t parse this ****');
+    stack = stack.split('\n').map(function (line) {
+
+      return {
+        file: line
+      };
+
+    });
+
+  }
+
+  return stack;
+
+};
