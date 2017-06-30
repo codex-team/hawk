b3c9925df6258cf942266d35f71e28d7d47e27a7
Taly Guryn <vitalik7tv@yandex.ru>
2017-06-30 21:03:14 +0300

    Remove title

--- hawk/routes/yard/index.js
+++ hawk/routes/yard/index.js
@@ -4,12 +4,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('yard/index', { title: 'Main page' });
+router.get('/', function (req, res, next) {
+
+  res.render('yard/index');
+
 });
 
-router.get('/docs', function(req, res, next) {
-  res.render('yard/docs/index', { title: 'Main page' });
+router.get('/docs', function (req, res, next) {
+
+  res.render('yard/docs/index');
+
 });
 
 module.exports = router;
