5152f8d8ec9beed6ba5712b95805e4f4b1ee8f77
Murod Khaydarov <murod.haydarov@gmail.com>
2017-07-14 12:38:06 +0300

    check all cases for /login route

--- hawk/tests/routes/auth.js
+++ hawk/tests/routes/auth.js
@@ -1,16 +1,23 @@
-let app = 'http://hawk.io:3000';
+let protocol = 'http',
+    domain   = 'hawk.io',
+    port     = '3000';
+    app      = protocol + '://' + domain + ':' + port;
 
 let chai = require('chai'),
     chaiHttp = require('chai-http'),
     expect = chai.expect,
+    assert = chai.assert,
     should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('/login page', function() {
+describe('Testing Routes: /login page', function() {
 
   /**
    * Open Login page
+   * 1) Request must return page with 200 status
+   * 2) Response mustn't contain any errors
+   * 3) Expect text/html content-type
    */
   it ('Check /login page', function(done) {
 
@@ -18,7 +25,8 @@ describe('/login page', function() {
         .get('/login')
         .end(function(err, res) {
 
-            expect(res.status).to.equal(200);
+            assert.equal(res.status, 200, 'Request returns page with 200 status')
+            expect(res.headers['content-type']).to.be.equal('text/html; charset=utf-8');
             expect(err).to.be.null;
             done();
 
@@ -26,44 +34,120 @@ describe('/login page', function() {
 
   });
 
-  it ('Test login page. Case 1: When user has correct cookies', function(done) {
+  /**
+   * User gets login page with correct cookies:
+   * If user has correct data, he must be redirected to /garage
+   */
+  it ('Case 1 #GET: User has correct cookies', function(done) {
 
     chai.request(app)
       .get('/login')
+      .set('Cookie', '_csrf=i1YNofMeM05axYRn52xpXQXr; user_id=5953d6cf3e89e4000f6eade4; user_hash=35ba436493f5a2cbb73071d1df31cfe2027ad2c338329d5d389acfdc3ec2071c')
       .end(function(err, res) {
-        // console.log("error", err);
-        // console.log("response", res);
 
+        expect(res.redirects[0]).to.be.equal(app + '/garage');
+        expect(err).to.be.null;
         done();
+
       });
 
   });
 
-  it ('Test login page. Case 2: When user has not cookies', function(done) {
+  /**
+   * If user doesn't have any cookie, he must see login page with form
+   * content-type must be text/html
+   */
+  it ('Case 2 #GET: User does not have cookies', function(done) {
 
     chai.request(app)
       .get('/login')
       .end(function(err, res) {
 
+        assert.equal(res.status, 200, 'Request returns page with 200 status')
+        assert.equal(res.redirects.length, 0, 'see login form');
+        expect(res.headers['content-type']).to.be.equal('text/html; charset=utf-8');
+        expect(err).to.be.null;
+
         done();
+
       });
 
   });
 
-  it ('Test login page. Case 3: When data is empty', function(done) {
-    done();
+  /**
+   * After authentification user must be redirected to the /garage
+   * content-type: text/html
+   */
+  it ('Case 3 #POST: Send correct data', function(done) {
+
+    let email    = 'morgan-_-95@mail.ru',
+        password = '22mrhpvi';
+
+    chai.request(app)
+      .post('/login')
+      .send({
+        'email' : email,
+        'password' : password
+      })
+      .end(function(err, res) {
+
+          expect(res.redirects[0]).to.be.equal(app + '/garage');
+          expect(err).to.be.null;
+          done();
+
+      });
+
   });
 
-  it ('Test POST request. Send correct data', function(done) {
-    done();
+  /**
+   * If POST data is not correct, show 'Try again' page
+   * content-type: text/html
+   */
+  it ('Case 4 #POST: Send incorrect data', function(done) {
+
+    let email    = 'morgan-_-95@mail.ru',
+        password = 'somepassword';
+
+    chai.request(app)
+      .post('/login')
+      .send({
+        'email' : email,
+        'password' : password
+      })
+      .end(function(err, res) {
+
+          assert.equal(res.status, 200, 'Request returns page with 200 status')
+          assert.equal(res.redirects.length, 0, 'see login form');
+          expect(res.headers['content-type']).to.be.equal('text/html; charset=utf-8');
+          expect(err).to.be.null;
+          done();
+
+      });
+
   });
 
-  it ('Test POST request. Send incorrect data', function(done) {
-    done();
-  });
+  /**
+   * Must refresh page or show login page
+   * content-type: text/html
+   */
+  it ('Case 5 #POST: Send empty data', function(done) {
 
-  it ('Test POST request. Send empty data', function(done) {
-    done();
+    chai.request(app)
+      .post('/login')
+      .send({
+        'email' : null,
+        'password' : null
+      })
+      .end(function(err, res) {
+
+          assert.equal(res.status, 200, 'Request returns page with 200 status')
+          assert.equal(res.redirects.length, 0, 'see login form');
+          expect(res.headers['content-type']).to.be.equal('text/html; charset=utf-8');
+          expect(err).to.be.null;
+          done();
+
+      });
+    
   });
 
 });
